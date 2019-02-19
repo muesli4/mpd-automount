8e36c99a2d310602872510166df378b2699e1314
Moritz Bruder <muesli4@gmail.com>
2019-02-19 11:05:53 +0100

    Add configuration support to link script

--- src/mpd-automount-link.sh
+++ src/mpd-automount-link.sh
@@ -12,6 +12,32 @@ usage() {
 }
 
 if [ $# -ge 3 ]; then
+    CONFIG_REL_PATH=mpd-automount/mpd-automount.conf
+    FIRST_CONFIG_PATH="$XDG_CONFIG_HOME/$CONFIG_REL_PATH"
+    if [ -n "$XDG_CONFIG_HOME" -a -e "$FIRST_CONFIG_PATH" ]; then
+        source "$FIRST_CONFIG_PATH"
+    else
+        SECOND_CONFIG_PATH="$HOME/.config/$CONFIG_REL_PATH"
+        if [ -e "$SECOND_CONFIG_PATH" ]; then
+            source "$SECOND_CONFIG_PATH"
+        else
+            init_config() {
+                DIR=$(dirname "$1")
+                mkdir -p "$DIR"
+                echo "Creating directory: $DIR"
+                echo "Writing config to $1."
+                echo -e "# trailing slash is not necessary\nMPD_LIBRARY_PATH=~/Music\nLINK_SUB_DIRECTORY=." > "$1"
+            }
+            if [ -n "$XDG_CONFIG_HOME" ]; then
+                CONFIG_PATH=$FIRST_CONFIG_PATH
+            else
+                CONFIG_PATH=$SECOND_CONFIG_PATH
+            fi
+            init_config "$CONFIG_PATH"
+            source "$CONFIG_PATH"
+        fi
+    fi
+
     LABEL="$2"
     UUID="$3"
     if [ -z "$LABEL" ]; then
@@ -19,11 +45,12 @@ if [ $# -ge 3 ]; then
     else
         MPD_MOUNT_NAME="${LABEL} ($UUID)"
     fi
-    MUSIC_DIR=~/Music
-    LINK_PATH="$MUSIC_DIR/$MPD_MOUNT_NAME"
+    LINK_PATH="$MPD_LIBRARY_PATH/$LINK_SUB_DIRECTORY/$MPD_MOUNT_NAME"
 
     refresh_library() {
-        mpc update "$MPD_MOUNT_NAME" > /dev/null
+        MPD_PATH="$LINK_SUB_DIRECTORY/$MPD_MOUNT_NAME"
+        MPD_PATH=${MPD_PATH#./}
+        mpc update "$MPD_PATH" > /dev/null
         # failed, maybe mpd doesn't run yet
         #if [ $? -eq 1 -a $1 -ge 0 ]; then
         #    sleep 1
@@ -33,7 +60,7 @@ if [ $# -ge 3 ]; then
 
     if [ "$1" == 'add' ]; then
         if [ $# -eq 4 ]; then
-            if [ -h "$LINK_PATH" -a "$(readlink \"$LINK_PATH\")" = "$MOUNT_POINT" ]; then
+            if [ -h "$LINK_PATH" -a "$(readlink -m \"$LINK_PATH\")" = "$(readlink -m \"$MOUNT_POINT\")" ]; then
                 echo "link already exists, doing nothing"
             else
                 MOUNT_POINT="$4"
