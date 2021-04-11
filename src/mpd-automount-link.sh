080ffda54546e2c2ee64f2d308fc6398cde2651d
Moritz Bruder <muesli4@gmail.com>
2021-04-11 16:29:52 +0200

    Use more secure bash variable syntax

--- src/mpd-automount-link.sh
+++ src/mpd-automount-link.sh
@@ -15,22 +15,22 @@ usage() {
 
 if [ $# -ge 3 ]; then
     CONFIG_REL_PATH=mpd-automount/mpd-automount.conf
-    FIRST_CONFIG_PATH="$XDG_CONFIG_HOME/$CONFIG_REL_PATH"
-    if [ -n "$XDG_CONFIG_HOME" -a -e "$FIRST_CONFIG_PATH" ]; then
-        source "$FIRST_CONFIG_PATH"
+    FIRST_CONFIG_PATH="${XDG_CONFIG_HOME}/${CONFIG_REL_PATH}"
+    if [ -n "${XDG_CONFIG_HOME}" -a -e "${FIRST_CONFIG_PATH}" ]; then
+        source "${FIRST_CONFIG_PATH}"
     else
-        SECOND_CONFIG_PATH="$HOME/.config/$CONFIG_REL_PATH"
+        SECOND_CONFIG_PATH="$HOME/.config/${CONFIG_REL_PATH}"
         if [ -e "$SECOND_CONFIG_PATH" ]; then
             source "$SECOND_CONFIG_PATH"
         else
             init_config() {
-                DIR=$(dirname "$1")
-                mkdir -p "$DIR"
-                echo "Creating directory: $DIR"
+                local DIR=$(dirname "$1")
+                mkdir -p "${DIR}"
+                echo "Creating directory: ${DIR}"
                 echo "Writing config to $1."
                 echo -e "# trailing slash is not necessary\nMPD_LIBRARY_PATH=~/Music\nLINK_SUB_DIRECTORY=." > "$1"
             }
-            if [ -n "$XDG_CONFIG_HOME" ]; then
+            if [ -n "${XDG_CONFIG_HOME}" ]; then
                 CONFIG_PATH=$FIRST_CONFIG_PATH
             else
                 CONFIG_PATH=$SECOND_CONFIG_PATH
@@ -43,16 +43,16 @@ if [ $# -ge 3 ]; then
     LABEL="$2"
     UUID="$3"
     if [ -z "$LABEL" ]; then
-        MPD_MOUNT_NAME="$UUID"
+        MPD_MOUNT_NAME="${UUID}"
     else
-        MPD_MOUNT_NAME="${LABEL} ($UUID)"
+        MPD_MOUNT_NAME="${LABEL} (${UUID})"
     fi
-    LINK_PATH="$MPD_LIBRARY_PATH/$LINK_SUB_DIRECTORY/$MPD_MOUNT_NAME"
+    LINK_PATH="${MPD_LIBRARY_PATH}/${LINK_SUB_DIRECTORY}/${MPD_MOUNT_NAME}"
 
     refresh_library() {
-        MPD_PATH="$LINK_SUB_DIRECTORY/$MPD_MOUNT_NAME"
-        MPD_PATH=${MPD_PATH#./}
-        mpc update "$MPD_PATH" > /dev/null
+        local MPD_PATH="${LINK_SUB_DIRECTORY}/${MPD_MOUNT_NAME}"
+        local MPD_PATH=${MPD_PATH#./}
+        mpc update "${MPD_PATH}" > /dev/null
         # failed, maybe mpd doesn't run yet
         #if [ $? -eq 1 -a $1 -ge 0 ]; then
         #    sleep 1
@@ -64,21 +64,21 @@ if [ $# -ge 3 ]; then
         if [ $# -eq 4 ]; then
             MOUNT_POINT="$4"
             # avoid quote escape quirks
-            A=$(readlink -m "$LINK_PATH")
-            B=$(readlink -m "$MOUNT_POINT")
-            if [ -h "$LINK_PATH" -a "$A" = "$B" ]; then
+            A=$(readlink -m "${LINK_PATH}")
+            B=$(readlink -m "${MOUNT_POINT}")
+            if [ -h "${LINK_PATH}" -a "$A" = "$B" ]; then
                 echo "link already exists, doing nothing"
             else
-                ln -s "$MOUNT_POINT" "$LINK_PATH" && refresh_library 5 \
-                    && echo "added $MOUNT_POINT to MPD database"
+                ln -s "${MOUNT_POINT}" "${LINK_PATH}" && refresh_library 5 \
+                    && echo "added ${MOUNT_POINT} to MPD database"
             fi
         else
             echo "missing mount point argument"
         fi
     elif [ "$1" == 'remove' ]; then
-        if [ -h "$LINK_PATH" ]; then
-            rm "$LINK_PATH" && refresh_library 5 \
-                && echo "removed $LINK_PATH from MPD database"
+        if [ -h "${LINK_PATH}" ]; then
+            rm "${LINK_PATH}" && refresh_library 5 \
+                && echo "removed ${LINK_PATH} from MPD database"
         else
             refresh_library 5
             echo "no link present, refreshing library anyway"
